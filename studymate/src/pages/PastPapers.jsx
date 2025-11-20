c1fc46d153110b189072dce9c5283f0a04cba92a
Bantu-Bethu Beya <90983460+ChocTrippleB@users.noreply.github.com>
2025-11-20 12:33:52 +0200

    pages

---
+++ studymate/src/pages/PastPapers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pastpapers = () => {
+  return (
+    <div>pastpapers</div>
+  )
+}
+
+export default pastpapers
---
+++ studymate/src/pages/StudyPlan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const studyplan = () => {
+  return (
+    <div>studyplan</div>
+  )
+}
+
+export default studyplan
